932d512e17c1a5f97137362401c97d757cfde2a4
Sam Cox <sjamcox@gmail.com>
2022-02-02 20:35:26 -0800

    Add put and delete routes to roast endpoint

--- pages/api/roasts/[id].js
+++ pages/api/roasts/[id].js
@@ -4,19 +4,46 @@ import Roast from 'models/Roast'
 
 export default async function handler(req, res) {
   const { body, method } = req
-  const { id } = req.query
+  const { id: _id } = req.query
 
   const session = await getSession({ req })
   if (!session) {
     return res.status(403).json({ success: false })
   }
 
+  const { email } = session.user
   await dbConnect()
 
   if (method === 'GET') {
     try {
-      const roast = await Roast.findById(id)
-      if (roast.email !== session.user.email) {
+      const roast = await Roast.findOne({ _id, email })
+      if (!roast) {
+        return res.status(400).json({ success: false })
+      }
+      res.status(200).json(roast)
+    } catch (error) {
+      res.status(400).json({ success: false })
+    }
+  }
+
+  if (method === 'PUT') {
+    try {
+      const roast = await Roast.findOneAndUpdate({ _id, email }, body, {
+        new: true,
+      })
+      if (!roast) {
+        return res.status(400).json({ success: false })
+      }
+      res.status(200).json(roast)
+    } catch (error) {
+      res.status(400).json({ success: false })
+    }
+  }
+
+  if (method === 'DELETE') {
+    try {
+      const roast = await Roast.findOneAndDelete({ id, email })
+      if (!roast) {
         return res.status(400).json({ success: false })
       }
       res.status(200).json(roast)
